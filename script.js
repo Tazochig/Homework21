f871efee24bd7a1cab6fc656b06ba9307d948fef
Tazochig <chigladzetazo@gmail.com>
2024-11-05 07:07:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+// // 1;  ("3-11")
+// // for (let index = 0; index <= 15; index++) {
+// //   if (index > 5) {
+// //     console.log("დიდია 5-ზე");
+// //   } else {
+// //     console.log(index);
+// //   }
+// // }
+// 2;
+// let luwebi = [3, 6, 9, 12, 15];
+// for (let index = 0; index < luwebi; index++) {
+//   console.log(luwebi);
+// }
+// 3;
+// let striqoni = "hola que tal";
+// for (let index = 0; index < striqoni.length; index++) {
+//   if (striqoni.includes("a")) {
+//     console.log(striqoni[index]);
+//   }
+// }
+// 4;
+// let ferebi = ["red", "green", "blue", "yellow"];
+// for (let index = 0; index < ferebi.length; index++) {
+//   if (ferebi[index].length > 5) {
+//     console.log(ferebi[index]);
+//   }
+// }
+
+// 5;
+// for (let index = 1; index <= 50; index++) {
+//   if (index % 7 === 0) {
+//     console.log(index + " გაყოფადია 7-ზე");
+//   }
+// }
+// 6;
+// const luwi = [4, 8, 12, 16, 20];
+// for (let index = 0; index < luwi.length; index++) {
+//   console.log(luwi[index] * 2);
+// }
+// 7;
+// let word = "javascript";
+// for (let i = 0; i < word.length; i++) {
+//   if (word.includes("j")) {
+//     break;
+//   } else {
+//     console.log(word[i]);
+//   }
+// }
+// 8;
+// let xutianebi = [5, 10, 15, 20, 25];
+// for (let index = 0; index < xutianebi.length; index++) {
+//   if (xutianebi[index] % 5 === 0) {
+//     console.log(xutianebi[index]);
+//   }
+// }
+// 9;
+// let misalmeba = "hello world";
+// for (let index = 0; index < misalmeba.length; index++) {
+//   if (misalmeba[index] !== "o") {
+//     console.log(misalmeba[index]);
+//   }
+// }
+// 10;
+// let jami = [1, 2, 3, 4, 5, 6];
+// let nuli = 0;
+// for (let index = 0; index < jami.length; index++) {
+//   if (jami[index] % 2 === 0) {
+//     nuli += jami[index];
+//   }
+// }
+// console.log(nuli);
+// 11;
+// let tabula = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let hero = 10;
+// for (let index = 0; index < tabula.length; index++) {
+// }
+// 12;
+// let masivi = [2, 7, 4, 9, 5];
+// for (let index = 0; index < masivi.length; index++) {
+//   if (masivi[index] > 4) {
+//     console.log(masivi[index] * 3);
+//   } else {
+//     console.log(masivi[index]);
+//   }
+// }
+// 13;
+// for (let index = 0; index < 31; index++) {
+//   if (index > 10) {
+//     console.log(index + " salam");
+//   } else {
+//     console.log(index);
+//   }
+// }
+// 14;
+// let sitkva = "frontend";
+// for (let index = 0; index < 1; index++) {
+//   if (sitkva.includes("e")) {
+//     console.log("ნაპოვნია e");
+//   }
+// }
+// 15;
+// let asebi = [100, 200, 300, 400, 500];
+// for (let index = 0; index < asebi.length; index++) {
+//   if (asebi[index] < 250) {
+//     console.log(asebi[index]);
+//   }
+// }
